68793879e7c281a9b74f62d1edb33fad7d15dffb
gabrieledarrigo <darrigo.g@gmail.com>
2017-03-25 21:28:27 +0100

    Styles for header, navbar and a first mobile version

---
+++ src/main/java/org/ominidi/web/controller/WebController.java
@@ -0,0 +1,32 @@
+package org.ominidi.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+    @GetMapping("/manifesto")
+    public ModelAndView index()  {
+        ModelAndView modelAndView = new ModelAndView("manifest");
+        return modelAndView;
+    }
+
+    @GetMapping("/foto")
+    public ModelAndView photos()  {
+        ModelAndView modelAndView = new ModelAndView("photos");
+        return modelAndView;
+    }
+
+    @GetMapping("/attributions")
+    public ModelAndView attributions()  {
+        ModelAndView modelAndView = new ModelAndView("attributions");
+        return modelAndView;
+    }
+
+    @GetMapping("/downloads")
+    public ModelAndView downloads()  {
+        ModelAndView modelAndView = new ModelAndView("downloads");
+        return modelAndView;
+    }
+}
