a75506defb0da44eed738b5bb9a36f344c36821e
gabrieledarrigo <darrigo.g@gmail.com>
2017-03-28 07:52:24 +0200

    Attributions

--- src/main/java/org/ominidi/web/controller/WebController.java
+++ src/main/java/org/ominidi/web/controller/WebController.java
@@ -9,24 +9,28 @@ public class WebController {
     @GetMapping({"/", "/manifesto"})
     public ModelAndView index()  {
         ModelAndView modelAndView = new ModelAndView("manifest");
+        modelAndView.addObject("current", "manifest");
         return modelAndView;
     }
 
     @GetMapping("/foto")
     public ModelAndView photos()  {
         ModelAndView modelAndView = new ModelAndView("photos");
+        modelAndView.addObject("current", "photos");
         return modelAndView;
     }
 
     @GetMapping("/attributions")
     public ModelAndView attributions()  {
         ModelAndView modelAndView = new ModelAndView("attributions");
+        modelAndView.addObject("current", "attributions");
         return modelAndView;
     }
 
     @GetMapping("/downloads")
     public ModelAndView downloads()  {
         ModelAndView modelAndView = new ModelAndView("downloads");
+        modelAndView.addObject("current", "downloads");
         return modelAndView;
     }
 }
