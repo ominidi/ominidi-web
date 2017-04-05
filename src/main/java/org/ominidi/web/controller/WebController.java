97b25a756bb0856e1dcf61988fcf76ae93a6249e
gabrieledarrigo <darrigo.g@gmail.com>
2017-04-05 21:20:32 +0200

    Wtf

--- src/main/java/org/ominidi/web/controller/WebController.java
+++ src/main/java/org/ominidi/web/controller/WebController.java
@@ -1,6 +1,6 @@
 package org.ominidi.web.controller;
 
-import org.ominidi.web.viewmodel.SeoViewModel;
+//import org.ominidi.web.viewmodel.SeoViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class WebController {
 
-    private SeoViewModel seoViewModel;
-
-    @Autowired
-    public WebController(SeoViewModel seoViewModel) {
-        this.seoViewModel = seoViewModel;
-    }
+//    private SeoViewModel seoViewModel;
+//
+//    @Autowired
+//    public WebController(SeoViewModel seoViewModel) {
+//        this.seoViewModel = seoViewModel;
+//    }
 
     @GetMapping({"/", "/manifesto"})
     public ModelAndView index()  {
