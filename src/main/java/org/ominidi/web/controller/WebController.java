5dc6ef7d61dfc19c476fb0a4d30cca0b815af45b
gabrieledarrigo <darrigo.g@gmail.com>
2017-04-11 08:00:13 +0200

    Seo fuck yeah

--- src/main/java/org/ominidi/web/controller/WebController.java
+++ src/main/java/org/ominidi/web/controller/WebController.java
@@ -1,45 +1,58 @@
 package org.ominidi.web.controller;
 
 import org.ominidi.web.factory.SeoViewModelFactory;
+import org.ominidi.web.viewmodel.SeoViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+
 @Controller
 public class WebController {
     private SeoViewModelFactory seoViewModelFactory;
 
+    private SeoViewModel seoViewModel;
+
     @Autowired
-    public WebController(SeoViewModelFactory seoViewModelFactory) {
+    public WebController(SeoViewModelFactory seoViewModelFactory) throws IOException {
         this.seoViewModelFactory = seoViewModelFactory;
+        this.seoViewModel = seoViewModelFactory.getViewModel("config/seo.yml");
     }
 
     @GetMapping({"/", "/manifesto"})
-    public ModelAndView index()  {
+    public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("manifest");
-        modelAndView.addObject("current", "manifest");
+        modelAndView.addObject("current", "manifest")
+                .addObject("seo", seoViewModel.getPages().get("manifest"));
         return modelAndView;
     }
 
     @GetMapping("/foto")
-    public ModelAndView photos()  {
+    public ModelAndView photos() {
         ModelAndView modelAndView = new ModelAndView("photos");
-        modelAndView.addObject("current", "photos");
+        modelAndView.addObject("current", "photos")
+                .addObject("seo", seoViewModel.getPages().get("photos"));
+
         return modelAndView;
     }
 
     @GetMapping("/attributions")
-    public ModelAndView attributions()  {
+    public ModelAndView attributions() {
         ModelAndView modelAndView = new ModelAndView("attributions");
-        modelAndView.addObject("current", "attributions");
+        modelAndView.addObject("current", "attributions")
+                .addObject("seo", seoViewModel.getPages().get("attributions"));
+
         return modelAndView;
     }
 
     @GetMapping("/downloads")
-    public ModelAndView downloads()  {
+    public ModelAndView downloads() {
         ModelAndView modelAndView = new ModelAndView("downloads");
-        modelAndView.addObject("current", "downloads");
+        modelAndView.addObject("current", "downloads")
+                .addObject("seo", seoViewModel.getPages().get("downloads"));
+
         return modelAndView;
     }
 }
